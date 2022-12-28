5aedca7f3c6ca84fca8948b8c8e8a3c6746fff26
mincheol <mincheol@vt.edu>
2022-12-28 15:22:04 -0500

    Clean up the code

--- src/app.c
+++ src/app.c
@@ -35,32 +35,22 @@ int main(int argc, char **argv) {
     tag = seL4_MessageInfo_new(0, 0, 0, 1);
     seL4_SetMR(0, MSG_DATA);
 
-    
- /* TASK 8: send and wait for a reply */
-    /* hint 1: seL4_Call()
-     * seL4_MessageInfo_t seL4_Call(seL4_CPtr dest, seL4_MessageInfo_t msgInfo)
-     * @param dest The capability to be invoked.
-     * @param msgInfo The messageinfo structure for the IPC.  This specifies information about the message to send (such as the number of message registers to send).
-     * @return A seL4_MessageInfo_t structure.  This is information about the repy message.
-     *
-     * hint 2: send the endpoint cap using argv (see TASK 6 in the other main.c)
-     */
-    ZF_LOGF_IF(argc < 1,
-               "Missing arguments.\n");
+    /* check arguments and get endpoint */
+    ZF_LOGF_IF(argc < 1, "Missing arguments.\n");
     seL4_CPtr ep = (seL4_CPtr) atol(argv[0]);
+
+    /* get shared memory address */
     void *shared_mem = (void *) atol(argv[1]);
     printf("shared_mem: %#" PRIxPTR "\n", *(unsigned long *)shared_mem);
 
+    /* send and wait for a repliy */
     seL4_Call(ep, tag);
 
     /* check that we got the expected reply */
-    ZF_LOGF_IF(seL4_MessageInfo_get_length(tag) != 1,
-               "Length of the data send from root thread was not what was expected.\n"
-               "\tHow many registers did you set with seL4_SetMR, within the root thread?\n");
+    assert(seL4_MessageInfo_get_length(tag) == 1);
 
     msg = seL4_GetMR(0);
-    ZF_LOGF_IF(msg != ~MSG_DATA,
-               "Unexpected response from root thread.\n");
+    assert(msg == ~MSG_DATA);
 
     printf("process_2: got a reply: %#" PRIxPTR "\n", msg);
 
--- src/main.c
+++ src/main.c
@@ -75,32 +75,19 @@ int main(void) {
     simple_default_init_bootinfo(&simple, info);
 
     /* print out bootinfo and other info about simple */
-    simple_print(&simple);
+    //simple_print(&simple);
 
     /* create an allocator */
     allocman = bootstrap_use_current_simple(&simple, ALLOCATOR_STATIC_POOL_SIZE,
                                             allocator_mem_pool);
-    ZF_LOGF_IF(allocman == NULL, "Failed to initialize allocator.\n"
-               "\tMemory pool sufficiently sized?\n"
-               "\tMemory pool pointer valid?\n");
+    assert(allocman);
 
     /* create a vka (interface for interacting with the underlying allocator) */
     allocman_make_vka(&vka, allocman);
 
-    /* TASK 1: create a vspace object to manage our vspace */
-    /* hint 1: sel4utils_bootstrap_vspace_with_bootinfo_leaky()
-     * int sel4utils_bootstrap_vspace_with_bootinfo_leaky(vspace_t *vspace, sel4utils_alloc_data_t *data, seL4_CPtr page_directory, vka_t *vka, seL4_BootInfo *info)
-     * @param vspace Uninitialised vspace struct to populate.
-     * @param data Uninitialised vspace data struct to populate.
-     * @param page_directory Page directory for the new vspace.
-     * @param vka Initialised vka that this virtual memory allocator will use to allocate pages and pagetables. This allocator will never invoke free.
-     * @param info seL4 boot info
-     * @return 0 on succes.
-     */
+    /* create a vspace object to manage our vspace */
     error = sel4utils_bootstrap_vspace_with_bootinfo_leaky(&vspace, &data, simple_get_pd(&simple), &vka, info);
-    ZF_LOGF_IFERR(error, "Failed to prepare root thread's VSpace for use.\n"
-                  "\tsel4utils_bootstrap_vspace_with_bootinfo reserves important vaddresses.\n"
-                  "\tIts failure means we can't safely use our vaddrspace.\n");
+    assert(error == 0);
 
     /* fill the allocator with virtual memory */
     void *vaddr;
@@ -111,26 +98,13 @@ int main(void) {
     bootstrap_configure_virtual_pool(allocman, vaddr,
                                      ALLOCATOR_VIRTUAL_POOL_SIZE, simple_get_pd(&simple));
 
-    /* TASK 2: use sel4utils to make a new process */
-    /* hint 1: sel4utils_configure_process_custom()
-     * hint 2: process_config_default_simple()
-     * @param process Uninitialised process struct.
-     * @param vka Allocator to use to allocate objects.
-     * @param vspace Vspace allocator for the current vspace.
-     * @param priority Priority to configure the process to run as.
-     * @param image_name Name of the elf image to load from the cpio archive.
-     * @return 0 on success, -1 on error.
-     *
-     * hint 2: priority is in APP_PRIORITY and can be 0 to seL4_MaxPrio
-     * hint 3: the elf image name is in APP_IMAGE_NAME
-     */
+    /* use sel4utils to make a new process */
     sel4utils_process_t new_process;
     sel4utils_process_config_t config = process_config_default_simple(&simple, APP_IMAGE_NAME, APP_PRIORITY);
+    config = process_config_auth(config, simple_get_tcb(&simple));
+    config = process_config_priority(config, seL4_MaxPrio);
     error = sel4utils_configure_process_custom(&new_process, &vka, &vspace, config);
-    ZF_LOGF_IFERR(error, "Failed to spawn a new thread.\n"
-                  "\tsel4utils_configure_process expands an ELF file into our VSpace.\n"
-                  "\tBe sure you've properly configured a VSpace manager using sel4utils_bootstrap_vspace_with_bootinfo.\n"
-                  "\tBe sure you've passed the correct component name for the new thread!\n");
+    assert(error == 0);
 
     /* give the new process's thread a name */
     NAME_THREAD(new_process.thread.tcb.cptr, "dynamic-3: process_2");
@@ -138,86 +112,42 @@ int main(void) {
     /* create an endpoint */
     vka_object_t ep_object = {0};
     error = vka_alloc_endpoint(&vka, &ep_object);
-    ZF_LOGF_IFERR(error, "Failed to allocate new endpoint object.\n");
+    assert(error == 0);
 
     /*
      * make a badged endpoint in the new process's cspace.  This copy
      * will be used to send an IPC to the original cap
      */
 
-    /* TASK 3: make a cspacepath for the new endpoint cap */
-    /* hint 1: vka_cspace_make_path()
-     * void vka_cspace_make_path(vka_t *vka, seL4_CPtr slot, cspacepath_t *res)
-     * @param vka Vka interface to use for allocation of objects.
-     * @param slot A cslot allocated by the cspace alloc function
-     * @param res Pointer to a cspacepath struct to fill out
-     *
-     * hint 2: use the cslot of the endpoint allocated above
-     */
+    /* make a cspacepath for the new endpoint cap */
     cspacepath_t ep_cap_path;
     seL4_CPtr new_ep_cap = 0;
     vka_cspace_make_path(&vka, ep_object.cptr, &ep_cap_path);
 
-    /* TASK 4: copy the endpont cap and add a badge to the new cap */
-    /* hint 1: sel4utils_mint_cap_to_process()
-     * seL4_CPtr sel4utils_mint_cap_to_process(sel4utils_process_t *process, cspacepath_t src, seL4_CapRights rights, seL4_Word data)
-     * @param process Process to copy the cap to
-     * @param src Path in the current cspace to copy the cap from
-     * @param rights The rights of the new cap
-     * @param data Extra data for the new cap (e.g., the badge)
-     * @return 0 on failure, otherwise the slot in the processes cspace.
-     *
-     * hint 2: for the rights, use seL4_AllRights
-     * hint 3: for the badge value use EP_BADGE
-     */
+    /* copy the endpoint cap and add a badge to the new cap */
     new_ep_cap = sel4utils_mint_cap_to_process(&new_process, ep_cap_path, seL4_AllRights, EP_BADGE);
-    ZF_LOGF_IF(new_ep_cap == 0, "Failed to mint a badged copy of the IPC endpoint into the new thread's CSpace.\n"
-               "\tsel4utils_mint_cap_to_process takes a cspacepath_t: double check what you passed.\n");
+    assert(new_ep_cap != 0);
 
     printf("NEW CAP SLOT: %" PRIxPTR ".\n", ep_cap_path.capPtr);
 
-    /* TASK 5: spawn the process */
-    /* hint 1: sel4utils_spawn_process_v()
-     * int sel4utils_spawn_process_v(sel4utils_process_t *process, vka_t *vka, vspace_t *vspace, int argc, char *argv[], int resume)
-     * @param process Initialised sel4utils process struct.
-     * @param vka Vka interface to use for allocation of frames.
-     * @param vspace The current vspace.
-     * @param argc The number of arguments.
-     * @param argv A pointer to an array of strings in the current vspace.
-     * @param resume 1 to start the process, 0 to leave suspended.
-     * @return 0 on success, -1 on error.
-     */
-    /* hint 2: sel4utils_create_word_args()
-     * void sel4utils_create_word_args(char strings[][WORD_STRING_SIZE], char *argv[], int argc, ...)
-     * Create c-formatted argument list to pass to a process from arbitrarily long amount of words.
-     *
-     * @param strings empty 2d array of chars to populate with word strings.
-     * @param argv empty 1d array of char pointers which will be set up with pointers to
-     *             strings in strings.
-     * @param argc number of words
-     * @param ... list of words to create arguments from.
-     *
-     */
-    //new_ep_cap = sel4utils_mint_cap_to_process(&new_process, ep_cap_path,
-    //                                           seL4_AllRights, EP_BADGE);
-
+    /* set up shared memory */
     void *shared_mem = vspace_new_pages(&vspace, seL4_AllRights, 1, seL4_PageBits);
-    ZF_LOGF_IF(shared_mem == NULL, "Failed to allocate shared_mem\n");
+    assert(shared_mem);
+
     *(unsigned long *)shared_mem = 0xBEEFDEAD;
+
     void *app_shared_mem = vspace_share_mem(&vspace, &new_process.vspace, shared_mem, 1,
                                             seL4_PageBits, seL4_AllRights, true);
-    ZF_LOGF_IF(app_shared_mem == NULL, "Failed to share mem\n");
+    assert(app_shared_mem);
 
+    /* spawn the process */
     seL4_Word argc = 2;
     char string_args[argc][WORD_STRING_SIZE];
     char* argv[argc];
     sel4utils_create_word_args(string_args, argv, argc, new_ep_cap, app_shared_mem);
 
     error = sel4utils_spawn_process_v(&new_process, &vka, &vspace, argc, (char**) &argv, 1);
-    ZF_LOGF_IFERR(error, "Failed to spawn and start the new thread.\n"
-                  "\tVerify: the new thread is being executed in the root thread's VSpace.\n"
-                  "\tIn this case, the CSpaces are different, but the VSpaces are the same.\n"
-                  "\tDouble check your vspace_t argument.\n");
+    assert(error == 0);
 
     /* we are done, say hello */
     printf("main: hello world\n");
@@ -225,50 +155,24 @@ int main(void) {
     /*
      * now wait for a message from the new process, then send a reply back
      */
-
     seL4_Word sender_badge = 0;
     seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 0);
-    seL4_Word msg;
 
-    /* TASK 6: wait for a message */
-    /* hint 1: seL4_Recv()
-     * seL4_MessageInfo_t seL4_Recv(seL4_CPtr src, seL4_Word* sender)
-     * @param src The capability to be invoked.
-     * @param sender The badge of the endpoint capability that was invoked by the sender is written to this address.
-     * @return A seL4_MessageInfo_t structure
-     *
-     * hint 2: seL4_MessageInfo_t is generated during build.
-     * hint 3: use the badged endpoint cap that you minted above
-     */
-   tag = seL4_Recv(ep_cap_path.capPtr, &sender_badge);
+    /* wait for a message */
+    tag = seL4_Recv(ep_cap_path.capPtr, &sender_badge);
 
-   /* make sure it is what we expected */
-    ZF_LOGF_IF(sender_badge != EP_BADGE,
-               "The badge we received from the new thread didn't match our expectation.\n");
-
-    ZF_LOGF_IF(seL4_MessageInfo_get_length(tag) != 1,
-               "Response data from the new process was not the length expected.\n"
-               "\tHow many registers did you set with seL4_SetMR within the new process?\n");
-
+    /* make sure it is what we expected */
+    assert(sender_badge == EP_BADGE);
+    assert(seL4_MessageInfo_get_length(tag) == 1);
 
     /* get the message stored in the first message register */
-    msg = seL4_GetMR(0);
+    seL4_Word msg = seL4_GetMR(0);
     printf("main: got a message %#" PRIxPTR " from %#" PRIxPTR "\n", msg, sender_badge);
 
     /* modify the message */
     seL4_SetMR(0, ~msg);
 
-    /* TASK 7: send the modified message back */
-    /* hint 1: seL4_ReplyRecv()
-     * seL4_MessageInfo_t seL4_ReplyRecv(seL4_CPtr dest, seL4_MessageInfo_t msgInfo, seL4_Word *sender)
-     * @param dest The capability to be invoked.
-     * @param msgInfo The messageinfo structure for the IPC.  This specifies information about the message to send (such as the number of message registers to send) as the Reply part.
-     * @param sender The badge of the endpoint capability that was invoked by the sender is written to this address. This is a result of the Wait part.
-     * @return A seL4_MessageInfo_t structure.  This is a result of the Wait part.
-     *
-     * hint 2: seL4_MessageInfo_t is generated during build.
-     * hint 3: use the badged endpoint cap that you used for Call
-     */
+    /* send the modified message back */
     seL4_ReplyRecv(ep_cap_path.capPtr, tag, &sender_badge);
 
     return 0;
